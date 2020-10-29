d6a97a891d48168ab806d283bb8b07f49258923d
Felipe Dantas <62896397+felipedantas184@users.noreply.github.com>
2020-10-29 15:27:23 -0300

    Update index.tsx

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
+import backIcon from '../../assets/images/back.png';
+import graduation from '../../assets/images/graduation.png';
 
 import './styles.css';
 
@@ -15,10 +16,11 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
     return (
         <header className="page-header">
             <div className="top-bar-container">
-                <Link to="/">
-                        <img src={backIcon} alt="Voltar" />
+                <Link to="/" style={{alignSelf: 'flex-start', marginTop: -3}} >
+                        <img src={backIcon} alt="Voltar" style={{width: 30}} />
                 </Link>
-                    <img src={logoImg} alt="Proffy" />
+                    <h1 style={{fontFamily: 'Righteous'}}>MeuProf</h1>
+                    <img src={graduation} alt="proffy" style={{width: 30, height: 30}} />
             </div>
 
             <div className="header-content">
