6724a8f06c55a4016c10837361791ff1d57e144c
Felipe Dantas <62896397+felipedantas184@users.noreply.github.com>
2020-10-29 15:25:38 -0300

    Update index.tsx

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/images/logo.svg';
-import landingImg from '../../assets/images/landing.svg';
+import logoImg from '../../assets/images/frame.svg';
+import landingImg from '../../assets/images/frameLanding.svg';
 
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
